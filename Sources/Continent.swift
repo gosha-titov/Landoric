ac1d6b099faa7ffe3ac7cb785d6474cff6b8c7ca
gosha <gosha.titov@icloud.com>
2026-05-16 22:16:55 +0300

    feat: add `Continent` enum

---
+++ Sources/Continent.swift
@@ -0,0 +1,31 @@
+/// A continents of the world.
+/// - Important: Antarctica is excluded because it has no sovereign countries.
+public enum Continent: String, CaseIterable, Sendable {
+    case Asia
+    case Africa
+    case Europe
+    case NorthAmerica
+    case SouthAmerica
+    case Australia
+    // Antarctica has no countries
+}
+
+
+
+// MARK: - Behavior Extensions
+
+extension Continent {
+    
+    /// All countries located on this continent.
+    public var countries: [Country] {
+        return switch self {
+        case .Asia: Country.allAsian
+        case .Africa: Country.allAfrican
+        case .Europe: Country.allEuropean
+        case .NorthAmerica: Country.allNorthAmerican
+        case .SouthAmerica: Country.allSouthAmerican
+        case .Australia: Country.allAustralian
+        }
+    }
+    
+}
